9160c713a3b316612ebe95f4de19b15e8dc5c421
Ashan Praba <47048522+apraba05@users.noreply.github.com>
2024-08-19 12:36:38 -0400

    fix: vpc and security group tf formatting

--- main.tf
+++ main.tf
@@ -5,21 +5,21 @@ resource "aws_vpc" "sftp_vpc" {
 
 resource "aws_security_group" "sftp_sg" {
   name_prefix = var.sg_name_prefix
-  vpc_id = aws_vpc.sftp_vpc
+  vpc_id      = aws_vpc.sftp_vpc.id
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     # use your cidr blocks
-    cidr_blocks      = var.cidr_blocks_ipv4
+    cidr_blocks = var.cidr_blocks_ipv4
     # ipv6_cidr_blocks = ["::/0"]
     # optional you can either use IPv4 or IPv6 CIDR Blocks
   }
 }
 
 resource "aws_subnet" "sftp_subnet" {
-  vpc_id            = aws_vpc.sftp_vpc
+  vpc_id = aws_vpc.sftp_vpc.id
   # utilize your own cidr block
   cidr_block        = var.cidr_block
   availability_zone = var.region
@@ -27,16 +27,17 @@ resource "aws_subnet" "sftp_subnet" {
 
 resource "aws_key_pair" "key" {
   key_name   = var.key_name
-  public_key = "" 
+  public_key = ""
   # generate a public key and this key will be the only key users can utilize to SSH into the instance
 }
 
-resource "aws_instance" "web" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.sftp_subnet
-  security_groups =  aws_security_group.sftp_sg
-  key_name = aws_key_pair.key
+resource "aws_instance" "sftp_ec2_server" {
+  ami             = var.instance_ami
+  instance_type   = var.instance_type
+  subnet_id       = aws_subnet.sftp_subnet.id
+  key_name        = aws_key_pair.key.key_name
+  vpc_security_group_ids = [aws_security_group.sftp_sg.id]  # Use security_group_ids with the ID of the security group
+  
   tags = {
     Name = var.ec2_name
   }
@@ -47,15 +48,14 @@ resource "aws_instance" "web" {
   }
 
   connection {
-      type        = "ssh"
-      user        = var.linux_user  # Update based on the AMI's default user
-      private_key = file("~/{$username}/.ssh/id_rsa") 
-      host        = self.public_ip
+    type        = "ssh"
+    user        = var.linux_user # Update based on the AMI's default user
+    private_key = file("~/${var.linux_user}/.ssh/id_rsa")
+    host        = self.public_ip
   }
 
   provisioner "file" {
-    source = "./variables.tf"
+    source      = "./variables.tf"
     destination = var.remote_directory
   }
 }
-
